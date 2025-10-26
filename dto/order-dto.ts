18f6e7197b9b4da2f070f242d142eae8501db3bd
ekaterinadegtiareva <edegtiareva@evolution.com>
2025-10-27 00:08:28 +0200

    Change comment to string value

--- dto/order-dto.ts
+++ dto/order-dto.ts
@@ -6,7 +6,14 @@ export class OrderDto {
   comment: string
   id: number
 
-  constructor(status: string, courierId: number, customerName: string, customerPhone: string, comment: "string", id: number) {
+  constructor(
+    status: string,
+    courierId: number,
+    customerName: string,
+    customerPhone: string,
+    comment: string,
+    id: number,
+  ) {
     this.status = status
     this.courierId = courierId
     this.customerName = customerName
